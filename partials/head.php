8272f17d7a544f0a060eadfaa4423d4f3d4c2365
srdjo98 <45862306+srdjo98@users.noreply.github.com>
2020-01-25 17:10:18 +0100

    change

--- partials/head.php
+++ partials/head.php
@@ -9,8 +9,8 @@ require_once "functions.php";
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="custom.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" crossorigin="anonymous">
-    <link href="custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="custom.css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
-    <title>GDeal - cheap and good</title>
+    <title>GDeal - cheap and good </title>
 </head>
 <body class="bg-white">
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,7 +1,5 @@
-<style>
-
 </style>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg">
             <a href="index.php" class="navbar-brand">GDeal </a> 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
